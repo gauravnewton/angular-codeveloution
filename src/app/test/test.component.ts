0839b75b6bb9ccbfe63429846bb3211f800999cf
Gaurav Kumar <gaurav.mute@gmail.com>
2021-04-10 17:08:34 +0530

    class binding

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -12,11 +12,24 @@ import { Component, OnInit } from '@angular/core';
 
     <input [disabled]="isDisabled" type="text" value="imple text"/>
     <input bind-disabled="!isDisabled" type="text" value="imple text"/>
+
+    <p class="text-danger" [class]="textSuccess" > GIlli Gilli </p>
+    <p class="text-success"> GIlli Gilli </p>
+    <p [class.text-danger]="isError"> GIlli Gilli </p>
+    
   `,
   styles: [`
     div {
       background:#000;
       color: #fff;
+    }
+
+    .text-success {
+     color: green;
+    }
+
+    .text-danger {
+      color:red;
     }
   `]
 })
@@ -25,7 +38,9 @@ bled = false;
   public isDisabled = true;
   public name = "pagal";
   public baseUrl = window.location.href;
-
+  public textSuccess = "text-success";
+  public textDanger = "text-danger";
+  public isError = false;
   constructor() { }
 
   ngOnInit(): void {
